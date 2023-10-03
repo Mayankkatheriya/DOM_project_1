2016c1820421fe28698c5178f11e69eb3a912d99
Mayankkatheriya <mayankkatheriya4@gmail.com>
2023-10-03 15:35:55 +0530

    first Commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// Use the getElementById method to retrieve the target div element.
+// Properly store the fetched element in a variable for further manipulation.(10 Marks)
+let myDiv = document.getElementById("myDiv");
+
+// Change the background color of the fetched div element to yellow using the style.backgroundColor property. (10 Marks)
+myDiv.style.backgroundColor = "yellow";
+
+// Add a margin of 20 pixels to the element using the style.margin property.(10 Marks)
+myDiv.style.margin = "20px";
+
+// Apply a padding of 10 pixels using the style.padding property.(10 Marks)
+myDiv.style.padding = "10px";
+
+// Change the font size to 18 pixels using the style.fontSize property.(10 Marks)
+myDiv.style.fontSize = '18px';
+
+// Set the font weight to bold using the style.fontWeight property. (10 Marks)
+myDiv.style.fontWeight = 'bold';
+
+// Change the height of the element to 200 pixels using the style.height property. (10 Marks)
+myDiv.style.height= '200px';
+
+// Modify the width of the element to 300 pixels using the style.width property.(10 Marks)
+myDiv.style.width='300px';
+
+myDiv.style.color = "blue";
